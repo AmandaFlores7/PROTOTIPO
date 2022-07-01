c686088b0ef9106909d0e9ebc79c06facae49bde
Matias <56362054+Copetiny-hub@users.noreply.github.com>
2022-06-30 22:52:19 -0400

    Ver-Menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './Pages/inicio/inicio.component';
 import { VerMenuAdministradorComponent } from './Pages/ver-menu-administrador/ver-menu-administrador.component';
 import { VerMenuComponent } from './Pages/ver-menu/ver-menu.component';
+import {IrPedidoComponent} from './Pages/ver-menu/ir-pedido/ir-pedido.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'Inicio', pathMatch: 'full' },
   { path: 'Inicio', component: InicioComponent },
   { path: 'VerMenu', component: VerMenuComponent },
-  { path: 'VerMenuAdministrador', component: VerMenuAdministradorComponent },];
+  { path: 'VerMenuAdministrador', component: VerMenuAdministradorComponent },
+  { path: 'IrVerPedido', component: IrPedidoComponent},
+];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
