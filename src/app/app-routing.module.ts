1538b53b9ffd083f31649580f13751b2d47a1494
Esteban Fredes Reyes <esteban.f.r00@gmail.com>
2022-06-30 22:39:08 -0400

    Componente Editar item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
+import { isNgTemplate } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './Pages/inicio/inicio.component';
 import { VerMenuAdministradorComponent } from './Pages/ver-menu-administrador/ver-menu-administrador.component';
 import { VerMenuComponent } from './Pages/ver-menu/ver-menu.component';
+import { EditarItemComponent } from './Pages/ver-menu-administrador/editar-item/editar-item.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'Inicio', pathMatch: 'full' },
   { path: 'Inicio', component: InicioComponent },
   { path: 'VerMenu', component: VerMenuComponent },
-  { path: 'VerMenuAdministrador', component: VerMenuAdministradorComponent },];
+  { path: 'VerMenuAdministrador', component: VerMenuAdministradorComponent },
+  { path: 'EditarItem', component: EditarItemComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
