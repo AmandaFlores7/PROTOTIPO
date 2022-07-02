c11c6bc99e182eb4fe4f1aa172e0e5d694581086
Esteban Fredes Reyes <esteban.f.r00@gmail.com>
2022-07-01 20:07:23 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,17 @@ import { VerMenuComponent } from './Pages/ver-menu/ver-menu.component';
 import { VerMenuAdministradorComponent } from './Pages/ver-menu-administrador/ver-menu-administrador.component';
 import { InicioComponent } from './Pages/inicio/inicio.component';
 import { EditarItemComponent } from './Pages/ver-menu-administrador/editar-item/editar-item.component';
+import { IrPedidoComponent } from './Pages/ver-menu/ir-pedido/ir-pedido.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent},
+  { path: 'Inicio', component: InicioComponent },
+  { path: 'VerMenu', component: VerMenuComponent },
+  { path: 'VerMenuAdministrador', component: VerMenuAdministradorComponent },
+  { path: 'VerMenuAdministrador/:idItem', component: EditarItemComponent},
+  { path: 'IrVerPedido', component: IrPedidoComponent},
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +30,9 @@ import { EditarItemComponent } from './Pages/ver-menu-administrador/editar-item/
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
